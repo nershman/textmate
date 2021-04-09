bca0d98ddd1b97468ac0b15316098f055687b130
Sherman <saline@ucsd.edu>
2021-04-09 23:02:02 +0200

    add(?) back support for transparent backgrounds
    reverted commit 6ffd0f2e92466355de0e81ed915b8e32daa8a3de which removed support for transparent backgrounds due to problems in newer OSX versions

--- Frameworks/OakTextView/src/OakDocumentView.mm
+++ Frameworks/OakTextView/src/OakDocumentView.mm
@@ -330,6 +330,7 @@ static NSString* const kFoldingsColumnIdentifier  = @"foldings";
 {
 	if(theme_ptr theme = _textView.theme)
 	{
+		[[self window] setOpaque:!theme->is_transparent() && !theme->gutter_styles().is_transparent()];
 		[textScrollView setBackgroundColor:[NSColor colorWithCGColor:theme->background(to_s(self.document.fileType))]];
 		[textScrollView setScrollerKnobStyle:theme->is_dark() ? NSScrollerKnobStyleLight : NSScrollerKnobStyleDark];
 
